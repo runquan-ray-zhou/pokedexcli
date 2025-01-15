07472705bafb7b080ed3f2903bfd9146b9706b57
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2025-01-15 00:14:20 -0500

    Include function to add to cache and get cache

--- pokecache/pokecache_test.go
+++ pokecache/pokecache_test.go
@@ -4,6 +4,7 @@ import "testing"
 
 func TestCreateCache(t *testing.T) {
 	cache := NewCache()
+
 	if cache.cache == nil {
 		t.Error("cache is nil")
 	}
@@ -12,12 +13,39 @@ func TestCreateCache(t *testing.T) {
 func TestAddGetCache(t *testing.T) {
 	cache := NewCache()
 
-	cache.Add("key1", []byte("val1"))
-	actual, ok := cache.Get("key1")
-	if !ok {
-		t.Error("key1 not found")
+	cases := []struct {
+		inputKey string
+		inputVal []byte
+	}{
+		{
+			inputKey: "key1",
+			inputVal: []byte("val1"),
+		},
+		{
+			inputKey: "key2",
+			inputVal: []byte("val2"),
+		},
+		{
+			inputKey: "",
+			inputVal: []byte("val3"),
+		},
 	}
-	if string(actual) != "val1" {
-		t.Error("value doesn't match")
+
+	for _, cas := range cases {
+		cache.Add(cas.inputKey, cas.inputVal)
+		actual, ok := cache.Get(cas.inputKey)
+		if !ok {
+			t.Errorf("%s not found", cas.inputKey)
+			continue
+		}
+		if string(actual) != string(cas.inputVal) {
+			t.Errorf("%s does not match %s",
+				string(actual),
+				string(cas.inputVal),
+			)
+			continue
+		}
+
 	}
+
 }
