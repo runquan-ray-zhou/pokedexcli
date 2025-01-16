81989c1e24f9013468078b8f5e6436f84c975b9d
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2025-01-15 22:25:50 -0500

    Add catch command

--- repl.go
+++ repl.go
@@ -70,6 +70,11 @@ func getCommands() map[string]cliCommand {
 			description: "Lists the pokemon in a location areas ",
 			callback:    callbackExplore,
 		},
+		"catch": {
+			name:        "catch {pokemon_name}",
+			description: "Attempt to catch a pokemon and add it to your pokedex",
+			callback:    callbackCatch,
+		},
 		"exit": {
 			name:        "exit",
 			description: "Turns off the Pokedex",
